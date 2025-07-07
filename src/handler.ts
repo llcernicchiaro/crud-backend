1559a6792f5a8cdbcdffde354780b8bf5128b3f2
Lorenzo Cernicchiaro <lopescernicchiaro@gmail.com>
2025-07-07 17:54:37 -0300

    fix: revert a revert

--- src/handler.ts
+++ src/handler.ts
@@ -1,10 +1,12 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 
 export const hello: APIGatewayProxyHandler = async (event) => {
+  const stage = process.env.STAGE || 'unknown';
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from Serverless + TypeScript!',
+      message: `Hello from CRUD Backend - With Github Actions!`,
+      stage,
       input: event,
     }),
   };
